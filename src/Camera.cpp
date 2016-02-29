95dfe2c88fe521be082df43fe75a7464b6def980
logicallon <ultima623-github@yahoo.com>
2016-02-28 14:17:21 -1000

    commit

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -8,7 +8,7 @@ Camera()
 		if(gamePad.GetRawButton(8))
 		{
 			cameraRotate.Set(.6);
-			cameraTilt.Set(1.0);xxx
+			cameraTilt.Set(1.0);
 		}
 		else if(gamePad.GetRawButton(8) && cameraRotate.Get()==(0.6) && cameraTilt.Get()==(1.0))
 		{
